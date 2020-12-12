40955f9bca801ecbb4a9f01df4f3735c928fd070
VictorAcencio <gabriel_kjv@hotmail.com>
2020-12-12 15:31:19 -0500

    primer commit

---
+++ src/main/java/com/gabrielAcencio/web/RyderBackend/RyderBackendApplication.java
@@ -0,0 +1,15 @@
+package com.gabrielAcencio.web.RyderBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyderBackendApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RyderBackendApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
---
+++ src/test/java/com/gabrielAcencio/web/RyderBackend/RyderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielAcencio.web.RyderBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RyderBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
